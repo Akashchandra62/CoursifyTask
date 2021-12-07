5c30294ab675f368b506e093c42ce2e5fea560a8
Akashchandra62 <78550561+Akashchandra62@users.noreply.github.com>
2021-12-07 15:40:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,3 @@
-//SECTION THREE
-let section2CardsContainer = document.getElementsByClassName("section2-cards")[0];
-let section2Card = document.getElementsByClassName("section2-card");
-let cards ='';
-let cardDetails = [
-    {
-        "logo" : `<i class="fas fa-location-arrow fa-2x">`,
-        "title" : "Easy Customizable",
-        "details" : `Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet!.`
-    },
-    {
-        "logo" : `<i class="fas fa-layer-group fa-2x"></i>`,
-        "title" : "Modern & Slick",
-        "details" : `Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet!.`
-    },
-    {
-        "logo" : `<i class="fas fa-shield-alt fa-2x"></i>`,
-        "title" : "Trusted and secure",
-        "details" : `Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet!.`
-    }
-]
-
-for(let i=0; i<3; i++)
-{
-    cards += `<div class="section2-card">
-    ${cardDetails[i].logo}</i>
-    <h3> ${cardDetails[i].title} </h3>
-    <p>${cardDetails[i].details}</p>
-</div>`;
-}
-
-section2CardsContainer.innerHTML = cards;
 
 //SECTION THREE
 let section3img = document.getElementById("section3image");
